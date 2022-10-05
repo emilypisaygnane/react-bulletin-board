88557c54441613cd973b846283edb68f5b5ad411
Emily Pisaygnane <episaygnane@me.com>
2022-10-05 15:38:35 -0700

    created functions to create posts, and fetch all posts

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <main className="home">
-          <h1>Bulletin Board</h1>
-        </main>
-      </Route>
-    </Switch>
+    <>
+      <Switch>
+        <Route exact path="/">
+          <main className="home">
+            <h1>Bulletin Board</h1>
+          </main>
+        </Route>
+      </Switch>
+      
+      <button className="create" onClick={createPost}>Create New Post</button>
+    </>
   );
 }
 
---
+++ src/services/fetchUtils.js
@@ -0,0 +1,17 @@
+import { checkError, client } from './client';
+
+export async function getPosts() {
+  const response = await client
+    .from('postmodern')
+    .select('*');
+
+  return checkError(response);
+}
+
+export async function createPosts(email, title, description) {
+  const response = await client
+    .from('postmodern')
+    .insert({ email, title, description });
+
+  return checkError(response);
+}
